9d2f0673e7827e658e43056daf51c0852ef70897
Adise211 <101215614+Adise211@users.noreply.github.com>
2022-12-03 08:40:09 +0200

    some style to extra style to experience

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,7 +1,8 @@
-// import { Dialog } from "@mui/material";
 import React from 'react';
 import OppositeContentTimeline from "../timeline/TimeLine.js";
+import { FaAngleDoubleLeft } from "react-icons/fa";
 import './experience.css';
+
 
 const Experience = () => {
     return (
@@ -14,12 +15,16 @@ const Experience = () => {
 
             <div className='e-right'>
                 <div className='e-wrapper'> 
-                    {/* <div className="jobDesc"></div> */}
                     <div 
                         className="open-text" 
                         style={{ fontSize: 20 }}
                     >
                         Please click on the comapny name to see more details
+                    </div>
+                    <div className='arrows'>
+                        <FaAngleDoubleLeft style={{ fontSize: 28 }}/>
+                        <FaAngleDoubleLeft style={{ fontSize: 28 }}/>
+                        <FaAngleDoubleLeft style={{ fontSize: 28 }}/>
                     </div>
                 </div>
             </div>
