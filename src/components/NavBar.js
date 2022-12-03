4412c106d9bdf0f569fb3edcb767eaae6e3dfc26
Adise211 <101215614+Adise211@users.noreply.github.com>
2022-12-03 08:54:02 +0200

    added logo and website name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import myIcon from '../images/myIcon.jpeg';
 
 
 const NavBar = () => {
@@ -9,7 +10,19 @@ const NavBar = () => {
   return (
       <div>
         <Navbar bg="black" variant="dark" fixed='top' className='smooth-scroll'>
-          <Container style={{ width: 600, paddingLeft: 80 }}>
+          <Container style={{ width: 800, paddingLeft: 120 }}>
+          <Navbar.Brand>
+            <img 
+              src={myIcon} 
+              alt='myLogo'
+              style={{ 
+                height: 50, 
+                width: 50, 
+                border: '1px white solid', 
+                borderRadius: 50
+              }}
+            />
+          </Navbar.Brand>
           <Navbar.Brand href="#intro" style={{color:'white'}}>Who I Am</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#skills" style={{color:'white'}}>Skills</Nav.Link>
